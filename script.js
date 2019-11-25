1465f13b489cc0e30a5a43cf7fdccea857fa3d95
Charles Max Crandall <robloxatme.com@gmail.com>
2019-11-25 15:59:06 -0500

    Added the rest of the positions in the script.js

--- script.js
+++ script.js
@@ -66,7 +66,162 @@ function position2() {
     grid[1] = "O";
 
   }
-  box2.removeEventListener("click", position2);
 
   switchPlayer();
+  box2.removeEventListener("click", position2);
+
+}
+
+//Position 3
+let box3 = document.querySelector("th.Position3");
+box3.addEventListener("click", position3);
+
+function position3() {
+  if (turn == 1) {
+
+    box3.textContent = "X";
+    grid[2] = "X";
+
+  } else {
+
+    box3.textContent = "O";
+    grid[2] = "O";
+
+  }
+
+  switchPlayer();
+  box3.removeEventListener("click", position3);
+
+}
+
+//Position 4
+let box4 = document.querySelector("th.Position4");
+box4.addEventListener("click", position4);
+
+function position4() {
+  if (turn == 1) {
+
+    box4.textContent = "X";
+    grid[3] = "X";
+
+  } else {
+
+    box4.textContent = "O";
+    grid[3] = "O";
+
+  }
+
+  switchPlayer();
+  box4.removeEventListener("click", position4);
+
+}
+
+//Position 5
+let box5 = document.querySelector("th.Position5");
+box5.addEventListener("click", position5);
+
+function position5() {
+  if (turn == 1) {
+
+    box5.textContent = "X";
+    grid[4] = "X";
+
+  } else {
+
+    box5.textContent = "O";
+    grid[4] = "O";
+
+  }
+
+  switchPlayer();
+  box5.removeEventListener("click", position5);
+
+}
+
+//Position 6
+let box6 = document.querySelector("th.Position6");
+box6.addEventListener("click", position6);
+
+function position6() {
+  if (turn == 1) {
+
+    box6.textContent = "X";
+    grid[5] = "X";
+
+  } else {
+
+    box6.textContent = "O";
+    grid[5] = "O";
+
+  }
+
+  switchPlayer();
+  box6.removeEventListener("click", position6);
+
+}
+
+//Position 7
+let box7 = document.querySelector("th.Position7");
+box7.addEventListener("click", position7);
+
+function position7() {
+  if (turn == 1) {
+
+    box7.textContent = "X";
+    grid[6] = "X";
+
+  } else {
+
+    box7.textContent = "O";
+    grid[6] = "O";
+
+  }
+
+  switchPlayer();
+  box7.removeEventListener("click", position7);
+
+}
+
+//Position 8
+let box8 = document.querySelector("th.Position8");
+box8.addEventListener("click", position8);
+
+function position8() {
+  if (turn == 1) {
+
+    box8.textContent = "X";
+    grid[7] = "X";
+
+  } else {
+
+    box8.textContent = "O";
+    grid[7] = "O";
+
+  }
+
+  switchPlayer();
+  box8.removeEventListener("click", position8);
+
+}
+
+//Position 9
+let box9 = document.querySelector("th.Position9");
+box9.addEventListener("click", position9);
+
+function position9() {
+  if (turn == 1) {
+
+    box9.textContent = "X";
+    grid[8] = "X";
+
+  } else {
+
+    box9.textContent = "O";
+    grid[8] = "O";
+
+  }
+
+  switchPlayer();
+  box9.removeEventListener("click", position9);
+
 }
