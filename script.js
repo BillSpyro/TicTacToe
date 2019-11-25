cc8ed89e6dc158f6310307b62413e9a721bd91ab
Charles Max Crandall <robloxatme.com@gmail.com>
2019-11-25 15:26:05 -0500

    Added a switchPlayer function that switches players and started code for first position

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
 let grid = [" ", " ", " ",
-            " ", " ", " ",
-            " ", " ", " "];
+  " ", " ", " ",
+  " ", " ", " "
+];
 let turn = 0;
 const players = ["O", "X"];
+
+//Switch players
+function switchPlayer() {
+
+  if (turn == 0) {
+
+    turn = 1;
+    return turn;
+
+  } else {
+
+    turn = 0;
+    return turn;
+
+  }
+
+}
+
+//Position 1
+let square = document.querySelector("th.Position1");
+square.addEventListener("click", event => {
+  if (turn == 1) {
+
+    square.textContent = "X";
+    grid[0] = "X";
+
+  } else {
+
+    square.textContent = "O";
+    grid[0] = "O";
+
+  }
+
+  switchPlayer;
+});
